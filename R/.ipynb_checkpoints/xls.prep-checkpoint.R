b88d1797c075184a40a5296224733267e89f17f5
Samet Sökel <64185287+sametsoekel@users.noreply.github.com>
2022-03-10 21:10:51 +0300

    cran revising

--- R/.ipynb_checkpoints/xls.prep-checkpoint.R
+++ R/.ipynb_checkpoints/xls.prep-checkpoint.R
@@ -6,16 +6,35 @@
 #' @param formula An object of class "formula": a symbolic description of the model to be fitted.
 #' @param data A data.frame object.
 #' @param dependent_var A character which is the same as left hand side variable in specified formula.
+#'
+#' @return A `list` object which contains a `data.frame` object to be modeled and character vector of independent variables.
+#'
 #' @export
 
 xls.prep <- function(formula,data,dependent_var){
     
-    matrix.baked <- stats::model.matrix(formula,data)
+    matrix.baked <- stats::model.matrix.lm(formula,data,na.action = 'na.pass')
     
     df.baked <- base::as.data.frame(matrix.baked)
     
     base::rm(matrix.baked)
     
+    df.baked[[dependent_var]] <- data[[dependent_var]]
+    
+    if(base::anyNA(df.baked)){
+
+        count_na <- base::nrow(df.baked[!stats::complete.cases(df.baked),])
+        msg_format <- '%s rows with missing data were dropped.'
+        base::message(base::sprintf(msg_format,count_na))
+
+        df.baked <- df.baked[stats::complete.cases(df.baked),]
+
+    }
+
+    dependent_var_vector <- df.baked[[dependent_var]]
+
+    df.baked[[dependent_var]] <- NULL
+        
     varnum <- base::ncol(df.baked)
     
     varnames <- base::colnames(df.baked)
@@ -36,7 +55,7 @@ xls.prep <- function(formula,data,dependent_var){
     
     base::eval(base::parse(text = base::sprintf('df.baked[["error_symbolic"]] <- %s',symbolic_error_calculation)))
     
-    df.baked[["error_symbolic"]] <- base::sprintf('(%s - %s)^2',df.baked[["error_symbolic"]],data[[dependent_var]])
+    df.baked[["error_symbolic"]] <- base::sprintf('(%s - %s)^2',df.baked[["error_symbolic"]],dependent_var_vector)
 
     base::list(data=df.baked,independent_var=varnames)
 
